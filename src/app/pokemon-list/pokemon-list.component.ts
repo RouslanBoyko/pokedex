a05c0d71008fd211d2dc474a76e9137de7b47e69
Ruslan <rouslanboyko.webdeveloper@gmail.com>
2023-11-13 15:04:10 +0100

    pagination done

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -9,32 +9,56 @@ import { DataService } from '../services/data.service';
 export class PokemonListComponent {
 
   pokemons : any[] = [];
+  offset = 0;
+  limit = 10;
   
   constructor(
     private dataService: DataService
   ){ }
 
   ngOnInit(): void {
-    this.dataService.getPokemons()
+    this.dataService.getPokemons(this.offset,this.limit)
       .subscribe((response: any) => {
         (response.results as Array<{ name: string }>).forEach(result => {
           this.dataService.getMoreData(result.name)
             .subscribe((uniqResponse: any) => {
               this.pokemons.push(uniqResponse);
+              this.pokemons.sort((a, b) => a.id - b.id);
               console.log(this.pokemons);
             });
         });
       });
     }
 
-    OnNextData(){
-      this.dataService.getPokemons()
+    next(){
+      this.offset += 10
+      this.pokemons = [];
+      this.dataService.OnNextData(this.offset,this.limit)
       .subscribe((response: any) => {
         (response.results as Array<{ name: string }>).forEach(result => {
           this.dataService.getMoreData(result.name)
             .subscribe((uniqResponse: any) => {
               this.pokemons.push(uniqResponse);
+              this.pokemons.sort((a, b) => a.id - b.id);
               console.log(this.pokemons);
+              
+            });
+        });
+      });
+    }
+
+    previous(){
+      this.offset -= 10
+      this.pokemons = [];
+      this.dataService.OnPreviousData(this.offset,this.limit)
+      .subscribe((response: any) => {
+        (response.results as Array<{ name: string }>).forEach(result => {
+          this.dataService.getMoreData(result.name)
+            .subscribe((uniqResponse: any) => {
+              this.pokemons.push(uniqResponse);
+              this.pokemons.sort((a, b) => a.id - b.id);
+              console.log(this.pokemons);
+              
             });
         });
       });
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,17 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-
+  private apiUrl = 'https://pokeapi.co/api/v2/pokemon';
   constructor(
     private http: HttpClient
   ) { }
 
   // get pokemons 
-  getPokemons(){
-    return this.http.get(`https://pokeapi.co/api/v2/pokemon?&limit=20`);
+
+  getPokemons(offset: number, limit: number): Observable<any> {
+    const url = `${this.apiUrl}?offset=${offset}&limit=${limit}`;
+    return this.http.get(url);
+  }
+
+  OnNextData(offset: number, limit: number): Observable<any>{
+    const url = `${this.apiUrl}?offset=${offset}&limit=${limit}`;
+    return this.http.get(url);
+  }
+
+  OnPreviousData(offset: number, limit: number): Observable<any>{
+    const url = `${this.apiUrl}?offset=${offset}&limit=${limit}`;
+    return this.http.get(url);
   }
 
   // get more pokemons data
